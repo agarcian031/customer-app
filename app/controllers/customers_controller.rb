ec5af6df54c37220b6d41f88c975c9fa8b6e9ad0
Amanda Garcia <algarcian@outlook.com>
2019-05-23 13:37:27 -0600

    Finish forms and views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,18 +12,38 @@ class CustomersController < ApplicationController
   end
 
   def new
+    @customer = current_user.customers.new 
   end
 
   def create
+    #Active Record 
+    # @customer = current_user.customers.new(customer_params)
+
+    #SQL 
+    @customer = Customer.create_customer(customer_params, current_user.id)
+    redirect_to customers_path
   end 
 
   def edit
   end
 
-  def update 
+  def update
+    #ACTIVE RECORD 
+    # @customer.update(customer_params)
+
+    #SQL  
+    Customer.update_customer(@customer.id, customer_params)
+    redirect_to customer_path(@customer)
   end 
 
   def destroy 
+    #ACTIVE RECORD 
+    # @customer.destroy 
+
+    #SQL 
+    Customer.delete_customer(@customer.id)
+    redirect_to customers_path
+
   end 
 
   private
@@ -34,4 +54,8 @@ class CustomersController < ApplicationController
     #SQL 
     @customer = Customer.single_customer(current_user.id, params[:id])
   end 
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -21,6 +21,39 @@ class Customer < ApplicationRecord
   end 
   #needs to be in order! customer id is being passed first, then user. 
 
+  #Class - create_customer 
+  #p - params 
+  def self.create_customer(p, user_id)
+    Customer.find_by_sql(["
+      INSERT INTO customers (first_name, last_name, email, phone, user_id, created_at, updated_at)
+      VALUES (:first, :last, :email, :phone, :user_id, :created_at, :updated_at);
+    ", {
+      first: p[:first_name], 
+      last: p[:last_name], 
+      email: p[:email],
+      phone: p[:phone],
+      user_id: user_id, 
+      created_at: DateTime.now, 
+      updated_at: DateTime.now
+    }])
+  end 
+
+  #Class - update customer 
+  def self.update_customer(customer_id, p)
+    Customer.find_by_sql(["
+      UPDATE customers AS c 
+      SET first_name = ?, last_name = ?, email = ?, phone = ?, updated_at = ? 
+      WHERE c.id = ? 
+    ", p[:first_name], p[:last_name], p[:email], p[:phone], DateTime.now, customer_id])
+  end 
+
+  #CLASS - DELETE customer 
+  def self.delete_customer(customer_id)
+    Customer.find_by_sql(["
+      DELETE FROM customers AS c 
+      WHERE c.id = ? 
+    ", customer_id])
+  end 
 
   #Intance Method for Full Name 
   def full_name 
